11733dfde9cd7932c96748b0ce9e36ca1680d75c
Arnab Daw <arnabdaw1@gmail.com>
2026-02-26 11:28:22 +0000

    Initial React CI/CD project

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="container">
+      <div className="card">
+        <h1>DevOps CI/CD Practice</h1>
+        <p>
+          This website is built using React + Vite and automated using
+          GitHub Actions.
+        </p>
+        <button>Explore More</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
